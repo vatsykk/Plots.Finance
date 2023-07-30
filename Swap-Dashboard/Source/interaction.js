2b78b9f2987c9ed8ece756c5899faafb15688798
Darius Tanz <scratchticketseb@gmail.com>
2023-07-30 03:58:11 +0000

    Jul 29, 2023, 11:58 PM

--- Swap-Dashboard/Source/interaction.js
+++ Swap-Dashboard/Source/interaction.js
@@ -88,8 +88,6 @@ async function Buy(){
     let gas = await contract.methods.Deposit().estimateGas({from: account, value: web3.utils.toWei(document.getElementById("ETH").value, 'ether')});
     let tx = await contract.methods.Deposit().send({from: account, value: web3.utils.toWei(document.getElementById("ETH").value, 'ether'), gas: gas});
 
-    ConfirmMint.style.display = "none";
-    WaitingTX.style.display = "";
     console.log(tx);
     return(tx);
 }
