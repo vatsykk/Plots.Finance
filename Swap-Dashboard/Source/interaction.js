d5e9532d9992de3c7663d8720e6a5645801d296a
Darius Tanz <scratchticketseb@gmail.com>
2023-07-30 15:10:06 +0000

    Jul 30, 2023, 11:10 AM

--- Swap-Dashboard/Source/interaction.js
+++ Swap-Dashboard/Source/interaction.js
@@ -97,7 +97,7 @@ async function Buy(){
 }
 
 async function Maximise(){
-    document.getElementById("ETH").value = (await getEtherBalance() - (gas * 0.000000021 * Ethprice).toLocaleString(undefined, { maximumFractionDigits: 2 }));
+    document.getElementById("ETH").value = (await getEtherBalance() - (gas * 0.000000021);
     Update(1);
     console.log(await getEtherBalance())
 }
