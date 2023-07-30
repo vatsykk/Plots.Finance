db15a345aded21b5b30826d6ac0bd7fb7c810563
Darius Tanz <scratchticketseb@gmail.com>
2023-07-30 04:00:05 +0000

    Jul 30, 2023, 12:00 AM

--- Swap-Dashboard/Source/interaction.js
+++ Swap-Dashboard/Source/interaction.js
@@ -4,6 +4,7 @@ let account;
 let netID;
 let LoggedIn = false;
 let Ethprice;
+let HyperLaneLink;
 fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd') .then(response => response.json()) .then(data => Ethprice = data.ethereum.usd) .then(() => console.log(Ethprice));
 
 document.getElementById("VLND").value = 0;
