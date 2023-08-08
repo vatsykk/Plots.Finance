62044bfaa37f41b13426c38cc51754ecaab43320
Darius Tanz <scratchticketseb@gmail.com>
2023-08-08 22:24:22 +0000

    Aug 8, 2023, 6:24 PM

--- Swap-Dashboard/Source/interaction.js
+++ Swap-Dashboard/Source/interaction.js
@@ -51,7 +51,9 @@ async function loginWithEth(){
         WalletButton.onclick = DisplayDisconnect;
         EstimateGas();
         let ContractBal = await (await web3.eth.getBalance("0x493fD82D18a17cF90a357aaD6A4c5B3D352427b0") / 1000000000000000000)
-        
+        if(ContractBal < 0){
+            
+        }
     } else { 
         alert("No ETHER Wallet available")
     }
