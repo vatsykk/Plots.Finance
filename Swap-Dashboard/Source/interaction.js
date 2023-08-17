fcea4bb5435c40fa214e146f145fd704194cd8d5
Darius Tanz <scratchticketseb@gmail.com>
2023-08-17 17:40:05 +0000

    Aug 17, 2023, 1:40 PM

--- Swap-Dashboard/Source/interaction.js
+++ Swap-Dashboard/Source/interaction.js
@@ -183,9 +183,9 @@ async function DisplayDisconnect(){
 }
 
 async function Buy(){
-    let gas = await contract.methods.Deposit().estimateGas({from: account, value: web3.utils.toWei(document.getElementById("ETH").value, 'ether')});
-    let tx = await contract.methods.Deposit().send({from: account, value: web3.utils.toWei(document.getElementById("ETH").value, 'ether'), gas: gas});
-    console.log(tx);
+  //  let gas = await contract.methods.Deposit().estimateGas({from: account, value: web3.utils.toWei(document.getElementById("ETH").value, 'ether')});
+  //  let tx = await contract.methods.Deposit().send({from: account, value: web3.utils.toWei(document.getElementById("ETH").value, 'ether'), gas: gas});
+   console.log(tx);
     EtherScanLaneLink = "https://etherscan.io/tx/" + tx.transactionHash;
     return(tx);
 }
