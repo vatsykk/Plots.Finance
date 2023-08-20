d55156fb649f808739cf224b53aac92297842a4b
KosmitoX <45701490+KosmitoX@users.noreply.github.com>
2023-08-20 20:55:35 -0300

    update

---
+++ Swap-Dashboard/Source/modal.js
@@ -0,0 +1,25 @@
+function openReviewMint() {
+    document.getElementById("ReviewMint").showModal();
+  }
+  function openSuccessful() {
+    document.getElementById("Successful").showModal();
+  }
+  function openWaiting() {
+    document.getElementById("Waiting").showModal();
+  }
+  function openFailed() {
+    document.getElementById("Failed").showModal();
+  }
+
+  function closeReviewMint() {
+    document.getElementById("ReviewMint").close();
+  }
+  function closeSuccessful() {
+    document.getElementById("Successful").close();
+  }
+  function closeWaiting() {
+    document.getElementById("Waiting").close();
+  }
+  function closeFailed() {
+    document.getElementById("Failed").close();
+  };
